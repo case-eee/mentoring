0f762660942cbbbe2f48edbda7b516a8b0d13958
Casey Ann Cumbow <cumbow8@gmail.com>
2016-12-03 18:15:28 -0700

    Refactor Github auth methods

--- app/controllers/browser_session_controller.rb
+++ app/controllers/browser_session_controller.rb
@@ -1,6 +1,5 @@
 class BrowserSessionController < ApplicationController
   def new
-    redirect_uri = "http://localhost:3000/auth"
-    @github_auth = "https://github.com/login/oauth/authorize?client_id=#{ENV['github_client_id']}&redirect_uri=#{redirect_uri}"
+    @github_auth_url = Github.authentication_url
   end
 end
--- app/models/github.rb
+++ app/models/github.rb
@@ -1,13 +1,15 @@
 class Github
   def self.authenticate(code)
-    redirect_uri = 'http://localhost:3000/auth'
-
     posting_url = "https://github.com/login/oauth/access_token"
-    posting_params = { client_id: ENV['github_client_id'], client_secret: ENV['github_client_secret'], code: code, redirect_uri: redirect_uri }
+    posting_params = { client_id: ENV['github_client_id'], client_secret: ENV['github_client_secret'], code: code, redirect_uri: ENV['github_redirect_url'] }
     resp = HTTParty.post(posting_url, query: posting_params)
     access_token = Rack::Utils.parse_nested_query(resp.parsed_response)['access_token']
     getting_url = "https://api.github.com/user?access_token=#{access_token}"
     user_attrs = HTTParty.get(getting_url)
     user_attrs
+  end
+
+  def self.authentication_url
+    "https://github.com/login/oauth/authorize?client_id=#{ENV['github_client_id']}&redirect_uri=#{ENV['github_redirect_uri']}"
   end
 end
